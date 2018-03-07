1d63c65309176766bef15e72f5a7cafa7d475e34
Craig Banach <craigsb93@gmail.com>
2018-03-07 23:16:39 +0000

    Added api filter to only get forecasts at noon.

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function GetWeatherData(city, countryCode) {
   var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&APPID=e37c2aae9d042422f66716be5e424ddf`;
 
   $.ajax(url)
-    .done(function() {
-      alert("success");
+    .done(function(data) {
+      console.log(data);
+      HandleApiData(data);
     })
     .fail(function() {
       alert("failure");
@@ -22,8 +23,19 @@ function GetWeatherData(city, countryCode) {
     })
 }
 
+function HandleApiData(data) {
+  var filtered = Object.filter(data.list, forecast => new Date(forecast.dt_txt).getHours() == 12);
+  console.log("filtered:");
+  console.log(filtered);
+}
+
 function CheckKeyPress(event) {
   if (event.keyCode == 13) {
     $('#submitButton').click();
   }
 }
+
+Object.filter = (obj, predicate) =>
+  Object.keys(obj)
+        .filter( key => predicate( obj[key] ))
+        .reduce( (res, key) => (res[key] = obj[key], res), {} );
